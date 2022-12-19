142d4c8280462bfb5d4f1ec02db12ceb171df6a9
arjunamarcelino <arjuna.marcelino@gdn-commerce.com>
2022-12-19 15:09:19 +0700

    implementation rghs

---
+++ main.py
@@ -0,0 +1,39 @@
+
+# encoding=utf-8
+import os
+import numpy as np
+import cv2
+import natsort
+
+from LabStretching import LABStretching
+from color_equalisation import RGB_equalisation
+from global_stretching_RGB import stretching
+from relativeglobalhistogramstretching import RelativeGHstretching
+
+np.seterr(over='ignore')
+if __name__ == '__main__':
+    pass
+
+folder = "C:/Users/OMEN/Downloads/Single-Underwater-Image-Enhancement-and-Color-Restoration-master/Single-Underwater-Image-Enhancement-and-Color-Restoration-master/Underwater Image Enhancement/RGHS"
+path = folder + "/InputImages"
+files = os.listdir(path)
+files =  natsort.natsorted(files)
+
+for i in range(len(files)):
+    file = files[i]
+    filepath = path + "/" + file
+    prefix = file.split('.')[0]
+    if os.path.isfile(filepath):
+        print('********    file   ********',file)
+        img = cv2.imread(folder +'/InputImages/' + file)
+
+        height = len(img)
+        width = len(img[0])
+
+        sceneRadiance = img
+
+        sceneRadiance = stretching(sceneRadiance)
+
+        sceneRadiance = LABStretching(sceneRadiance)
+
+        cv2.imwrite('OutputImages/' + prefix + '_RGHS.jpg', sceneRadiance)
